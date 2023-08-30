1d6017c6fdc6840cab0fe45908415b98099132b1
TheNjengaKarori <njenga.k11@gmail.com>
2023-08-30 09:28:40 +0100

    added merch page

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>We are a small team of passionate people who love creating and selling merch. We believe that everyone should be able to express themselves through their clothing, and we're here to help you do just that.
+
+We offer a wide variety of merch, from t-shirts and hoodies to hats and accessories. We also have a variety of designs to choose from, so you're sure to find something you love.
+
+We're committed to providing our customers with the best possible experience. That's why we offer free shipping on all orders over $50, a satisfaction guarantee, and a variety of other special offers.
+
+We're also committed to giving back to our community. That's why we donate a portion of our proceeds to charity.
+
+We hope you'll join us on our journey to create a more stylish and compassionate world.
+
+Thank you for shopping with us!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p{
+    margin: 20px 0;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>  
+        <h2>Home</h2>
+        <p>Welcome to our store! We're glad you're here. We offer free shipping on all orders over Ksh5000, a satisfaction guarantee, and a variety of other special offers. We're confident that you'll find something you love in our store.</p>
+        <p><bold>Here are just a few of the benefits of shopping with us:</bold></p>
+        <ul>
+        <li>Satisfaction guarantee: If you're not happy with your purchase, simply return it for a full refund</li>
+        <li>10% off your first order</li>
+        <li>Exclusive discounts for members</li>
+    </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p{
+    margin: 20px 0;
+}
+
+</style>
